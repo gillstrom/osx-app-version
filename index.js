d616bddcfd907f7476ce2e2bb64b6b221c1b1973
Andreas Gillström <andreasgillstrom@gmail.com>
2015-05-18 07:18:02 +0200

    Make sure path is .app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 var execFile = require('child_process').execFile;
+var fs = require('fs');
 
 module.exports = function (app, cb) {
 	var cmd = 'mdls';
@@ -8,6 +9,16 @@ module.exports = function (app, cb) {
 		'kMDItemVersion',
 		app
 	];
+
+	try {
+		if (!fs.statSync(app).isDirectory() || app.indexOf('.app') === -1) {
+			cb(new Error(false));
+			return;
+		}
+	} catch (e) {
+		cb(e);
+		return;
+	}
 
 	execFile(cmd, args, function (err, stdout, stderr) {
 		if (err) {
